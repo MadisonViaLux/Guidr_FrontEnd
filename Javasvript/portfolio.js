9d84b5baa17839d7b9bda33928778cda84b4da84
MadisonViaLux <madison.s.mckown@gmail.com>
2019-06-06 21:50:15 -0700

    all but a bit of js

--- Javasvript/portfolio.js
+++ Javasvript/portfolio.js
@@ -1,1 +1,20 @@
-const dropdowns = document
+class Dropdown{
+    constructor(element){
+
+        this.element = element;
+        this.button = this.element.querySelector('.dropdown-option');
+        this.content = document.querySelector('.dropdown-content');
+
+        this.button.addEventListener('click', () => this.toggleOptions());
+
+    }
+
+    toggleOptions(){this.content.classList.toggle('hide-dropdown')};
+}
+
+
+const dropdowns = document.querySelectorAll('.dropdowns');
+
+dropdowns.forEach(element => new Dropdown(element));
+
+
